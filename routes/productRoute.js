ef5e8d1f5d5e7f36529e8c0f1a04578d74704447
asna sharin <asnasherin1233@gmail.com>
2024-07-27 23:13:27 +0530

    feat: add create,update products controllers with cloudinary image handling

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createProductController } from "../controllers/productController.js";
+import { createProductController, getAllProduct, updateProduct } from "../controllers/productController.js";
 import { isAdmin, protect } from "../middlewares/authMiddleware.js";
 
 const route = Router();
 
 route.post("/create-product",protect, isAdmin, createProductController);
+route.get("/get-product", getAllProduct)
+route.put("/update-prod/:id", updateProduct)
 
 export default route;
