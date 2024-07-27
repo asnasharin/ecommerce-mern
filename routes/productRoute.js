d87849b277a01d0da5c4ff947e27238f5ece65d8
asna sharin <asnasherin1233@gmail.com>
2024-07-27 23:29:49 +0530

    feat: add delete controller

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createProductController, getAllProduct, updateProduct } from "../controllers/productController.js";
+import { createProductController, deleteProductController, getAllProduct, updateProduct } from "../controllers/productController.js";
 import { isAdmin, protect } from "../middlewares/authMiddleware.js";
 
 const route = Router();
@@ -7,5 +7,6 @@ const route = Router();
 route.post("/create-product",protect, isAdmin, createProductController);
 route.get("/get-product", getAllProduct)
 route.put("/update-prod/:id", updateProduct)
+route.delete("/delete-prod/:id", deleteProductController)
 
 export default route;
