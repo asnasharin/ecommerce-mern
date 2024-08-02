c9890aa59526a4eac66c49e18aba7998fd3f00dd
asna sharin <asnasherin1233@gmail.com>
2024-08-02 14:28:12 +0530

    feat: add signup reducer and actions

---
+++ client/src/Reducers/UserReducer.jsx
@@ -0,0 +1,66 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { 
+    REGISTER_USER_FAIL,
+    REGISTER_USER_SUCCESS,
+    REGISTER_USER_REQUEST
+ } from "../constants/userConstants"
+import { signup } from "../Actions/userActions";
+
+//  initail state
+const user = localStorage.getItem("user")
+ const initialState = {
+    loading: false,
+    isAuthenticted: false,
+    user: user ? JSON.parse(user) : null,
+    errorMessge: {
+        message: "",
+        status: null,
+    },
+ };
+
+ const userReducer = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        logout: (state) => {
+            state.user = null;
+            state.isAuthenticted = false;
+            localStorage.removeItem("user");
+            localStorage.removeItem("token");
+            window.location.reload();
+        },
+        clearErrors: (state) => {
+            state.errorMessge = {
+                message: "",
+                status: null,
+            };
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(signup.pending, (state) => {
+            state.loading = true;
+            state.errorMessge = {
+                message: null,
+                status: null,
+            };
+        })
+        .addCase(signup.fulfilled, (state, action) => {
+            state.loading = false;
+            state.isAuthenticted = true;
+            state.user = action.payload.user;
+            localStorage.setItem("user", JSON.stringify(action.payload.user));
+            localStorage.setItem("token", JSON.stringify(action.payload.token));
+        })
+        .addCase(signup.rejected, (state, action) => {
+            state.loading = false;
+            state.isAuthenticted = false;
+            state.errorMessge = action.payload;
+        });
+    },
+ });
+
+ export const { logout, reset } = userReducer.actions;
+
+ export default userReducer.reducer;
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import store from './redux/store.jsx'
+import { Provider } from "react-redux"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
