469f493e753c622ccf5b30b37a26b1aafa21be5f
asna sharin <asnasherin1233@gmail.com>
2024-08-12 19:57:55 +0530

    add product component

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,116 +1,97 @@
-// import React, {useEffect } from "react";
-// import "./Product.module.scss";
-// import { useDispatch, useSelector } from "react-redux";
-// // import Loader from "../layouts/loader/Loader";
-// // import { useAlert } from "react-alert";
-// // import { useRouteMatch } from "react-router-dom";
-// // import MetaData from "../layouts/MataData/MataData";
-// // import { clearErrors, getProduct } from "../../actions/productAction";
-// // import ProductCard from "../Home/ProductCard";
-// // import Pagination from "react-js-pagination";
-// // import Slider from "@mui/material/Slider";
-// import { Typography } from "@mui/material";
-// import Select from "@mui/material/Select";
-// import MenuItem from "@mui/material/MenuItem";
-// // import Button from "@mui/material/Button";
-// // import InventoryIcon from "@mui/icons-material/Inventory";
+import React, { useEffect } from 'react';
+import styles from './Product.module.scss';
+import { FormControlLabel, MenuItem, Radio, RadioGroup, Select, Slider, Typography } from '@mui/material';
+import { ArrowDropDown } from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { clearErrors, getProducts } from "../../Actions/productAction";
+import ProductCard from "../Home/ProductCard/ProductCard";
 
+function Product() {
+  const categories = ["men", "women", "kids", "furniture", "shoe", "perfumes"];
+  const { products, error } = useSelector((state) => state.products);
+  const dispatch = useDispatch();
 
-// // import Radio from "@mui/material/Radio";
-// // import RadioGroup from "@mui/material/RadioGroup";
-// // import FormControlLabel from "@mui/material/FormControlLabel";
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+    dispatch(getProducts());
+  }, [dispatch, error]);
 
-
-// import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-// function Product() {
-//   return (
-//     <div className="productPage">
-//               <div className="prodcutPageTop">
-//                 <div className="filterBox">
-//                   {/* Price */}
-//                   <div className="priceFilter">
-//                     <Typography
-//                       style={{
-//                         fontSize: "18px",
-//                         padding: "5px",
-//                         fontWeight: 700,
-//                         color: "#414141",
-//                       }}
-//                     >
-//                       Price
-//                     </Typography>
-//                     <div className="priceSlider">
-//                       {/* <Slider
-//                         value={price}
-//                         onChange={priceHandler}
-//                         min={0}
-//                         max={100000}
-//                         step={100}
-//                         valueLabelDisplay="auto"
-//                         aria-labelledby="range-slider"
-//                       /> */}
-//                     </div>
-//                     <div className="priceSelectors">
-//                       <div className="priceSelector">
-//                         <Select
-//                         //   value={price[0]}
-//                         //   onChange={(e) =>
-//                         //     setPrice([+e.target.value, price[1]])
-//                         //   }
-//                           className="priceOption"
-//                           IconComponent={ArrowDropDownIcon}
-//                         //   renderValue={(selected) =>
-//                         //     selected !== "" ? selected : "min"
-//                         //   } // Display "min" as default label
-//                         >
-//                           <MenuItem value={5000} className="menu_item">
-//                             5000
-//                           </MenuItem>
-//                           <MenuItem value={10000} className="menu_item">
-//                             10000
-//                           </MenuItem>
-//                           {/* Add more options as per your requirement */}
-//                         </Select>
-//                         <span className="toText">to</span>
-//                         <Select
-//                         //   value={price[1]}
-//                         //   onChange={(e) =>
-//                         //     setPrice([price[0], +e.target.value])
-//                         //   }
-//                           className="priceOption"
-//                           IconComponent={ArrowDropDownIcon}
-//                         //   renderValue={(selected) =>
-//                         //     selected !== "" ? selected : "max"
-//                         //   }
-//                         >
-//                           <MenuItem value={50000} className="menu_item">
-//                             50000
-//                           </MenuItem>
-//                           <MenuItem value={20000} className="menu_item">
-//                             20000
-//                           </MenuItem>
-//                           {/* Add more options as per your requirement */}
-//                         </Select>
-//                       </div>
-//                     </div>
-//                   </div>
-//                   </div>
-//                   </div>
-//                   </div>
-                  
-
-//   )
-// }
-
-// export default Product
-import React from 'react'
-
-const Product = () => {
   return (
-    <div>
-      
+    <div className={styles.productPage}>
+      <div className={styles.filterBox}>
+        {/* Price Filter */}
+        <div className={styles.priceFilter}>
+          <Typography style={{ fontSize: '18px', fontWeight: 700, color: '#414141' }}>
+            Price
+          </Typography>
+          <Slider min={0} max={100000} step={100} valueLabelDisplay='auto' />
+          <div className={styles.priceSelectors}>
+            <Select
+              className={styles.priceOption}
+              defaultValue={5000}
+              IconComponent={ArrowDropDown}
+            >
+              <MenuItem value={5000} className={styles.menu_item}>5000</MenuItem>
+              <MenuItem value={10000} className={styles.menu_item}>10000</MenuItem>
+            </Select>
+            <span className={styles.toText}>to</span>
+            <Select
+              className={styles.priceOption}
+              defaultValue={50000}
+              IconComponent={ArrowDropDown}
+            >
+              <MenuItem value={50000} className={styles.menu_item}>50000</MenuItem>
+              <MenuItem value={20000} className={styles.menu_item}>20000</MenuItem>
+            </Select>
+          </div>
+        </div>
+
+        <div className={styles.filter_divider}></div>
+
+        {/* Categories Filter */}
+        <div className={styles.categoriesFilter}>
+          <Typography style={{ fontSize: "18px", fontWeight: 700, color: "#414141" }}>
+            Categories
+          </Typography>
+          <ul className={styles.categoryBox}>
+            {categories.map((category, index) => (
+              <li key={index}>
+                <label htmlFor={`category-${index}`}>
+                  <input type="checkbox" id={`category-${index}`} />
+                  {category}
+                </label>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className={styles.filter_divider}></div>
+
+        {/* Ratings Filter */}
+        <div className={styles.ratingsFilter}>
+          <Typography style={{ fontSize: "18px", fontWeight: 700, color: "#414141" }}>
+            Ratings Above
+          </Typography>
+          <RadioGroup row>
+            <FormControlLabel value="4" control={<Radio />} label="4★ & above" />
+            <FormControlLabel value="3" control={<Radio />} label="3★ & above" />
+            <FormControlLabel value="2" control={<Radio />} label="2★ & above" />
+          </RadioGroup>
+        </div>
+      </div>
+
+      <div className={styles.productsContainer}>
+        <div className={products.length < 2 ? styles.products1 : styles.products}>
+          {products && products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
