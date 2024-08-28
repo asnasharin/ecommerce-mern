b340084eb5ef77d2d2ff7c0a7947ac6741656db0
asna sharin <asnasherin1233@gmail.com>
2024-08-28 22:13:02 +0530

    fix

--- client/src/Reducers/CartReducer.jsx
+++ client/src/Reducers/CartReducer.jsx
@@ -1,7 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { addToCart, removeFromCart } from "../Actions/CartActions";
 
-// Initialize cartItems from localStorage if available
 const storedCartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
 const initialState = {
     cartItems: storedCartItems,
@@ -29,21 +28,19 @@ const cartSlice = createSlice({
         .addCase(addToCart.fulfilled, (state, action) => {
             state.loading = false;
             const item = action.payload;
-            const existItem = state.cartItems.find((i) => i.productId === item.productId);
+    
+            const existItemIndex = state.cartItems.findIndex((i) => i.productId === item.productId);
+                console.log("existing item index", existItemIndex);
 
-            if (existItem) {
-                state.cartItems = state.cartItems.map((i) => 
-                    i.productId === existItem.productId ? item : i 
-                );
-            } else {
-                state.cartItems.push(item);
-            }
-
+                if (existItemIndex !== -1) {
+                    state.cartItems[existItemIndex].quantity = item.quantity;
+                } else {
+                    state.cartItems.push(item);
+                }
             localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
         })
         .addCase(addToCart.rejected, (state, action) => {
             state.loading = false;
-            console.error("Add to Cart Error:", action.payload); // Debugging log
             state.error = action.payload;
         })
         .addCase(removeFromCart.pending, (state) => {
@@ -52,15 +49,15 @@ const cartSlice = createSlice({
         })
         .addCase(removeFromCart.fulfilled, (state, action) => {
             state.loading = false;
-            console.log("Remove from Cart Payload:", action.payload); // Debugging log
+            console.log("Remove from Cart Payload:", action.payload); 
             state.cartItems = state.cartItems.filter((i) => i.productId !== action.payload);
 
-            console.log("Updated Cart Items after Remove:", state.cartItems); // Debugging log
+            console.log("Updated Cart Items after Remove:", state.cartItems); 
             localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
         })
         .addCase(removeFromCart.rejected, (state, action) => {
             state.loading = false;
-            console.error("Remove from Cart Error:", action.payload); // Debugging log
+            console.error("Remove from Cart Error:", action.payload); 
             state.error = action.payload;
         });
     }
