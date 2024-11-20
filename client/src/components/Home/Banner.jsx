c5bb053e1efe1f7fb9c190465d808967b60fa8dc
asna sharin <asnasherin1233@gmail.com>
2024-11-20 15:38:57 +0530

    feat: modified banner

--- client/src/components/Home/Banner.jsx
+++ client/src/components/Home/Banner.jsx
@@ -1,15 +1,86 @@
-// src/components/Banner.jsx
+import { useState } from "react";
+import Carousel from "react-material-ui-carousel";
+import { Button } from "@mui/material";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { Link } from "react-router-dom";
+import styles from "./Banner.module.scss";
 
-import React from 'react';
-import styles from './Banner.module.scss';
-import { Typography, Button, Container } from '@mui/material';
+const slides = [
+  {
+    image: "/Image/banner4.png",
+  },
+  {
+    image: "/Image/banner3.png",
+  },
+  {
+    image: "/Image/banner.png",
+  },
+  {
+    image: "/Image/banner2.png",
+  },
+];
 
-const Banner = ({ imageUrl, title, subtitle, buttonText, buttonLink }) => {
+export default function Banner() {
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => (prevActiveStep + 1) % slides.length);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => (prevActiveStep - 1 + slides.length) % slides.length);
+  };
+
   return (
-    <div className={styles.banner} style={{ backgroundImage: `url(${imageUrl})` }}>
-      <div className={styles.overlay}></div>
-    </div>
+    <>
+      <Carousel
+        autoPlay
+        navButtonsAlwaysVisible
+        indicators={false}
+        animation="slide"
+        interval={5000}
+        timeout={500}
+        cycleNavigation
+        navButtonsProps={{
+          style: {
+            backgroundColor: "#00000088",
+            borderRadius: 0,
+            padding: 0,
+            margin: 0,
+            height: "100%",
+          },
+        }}
+        prevButton={
+          <Button
+            className={styles.navButton}
+            onClick={handleBack}
+            startIcon={<ArrowBackIosIcon />}
+          ></Button>
+        }
+        nextButton={
+          <Button
+            className={styles.navButton}
+            onClick={handleNext}
+            endIcon={<ArrowForwardIosIcon />}
+          ></Button>
+        }
+        fullHeightHover={false}
+        className={styles.slide}
+      >
+        {slides.map((slide, index) => (
+          <div key={index} className={styles.slide}>
+            <img src={slide.image} alt="slider" className={styles.slideImage} />
+            <div className={styles.slideContent}>
+              <h2 className={styles.quote}>{slide.quote}</h2>
+              <h3 className={styles.saleText}>{slide.saleText}</h3>
+              <Link to="/products">
+                <Button className={styles.productButton}>{slide.productText}</Button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </Carousel>
+    </>
   );
-};
-
-export default Banner;
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import store from './redux/store.jsx'
 import { Provider } from "react-redux"
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <App />
